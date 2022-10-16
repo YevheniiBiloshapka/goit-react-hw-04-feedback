25032236d18051dff8305b8bb15f6c2d5fda3b81
YevheniiBiloshapka <106679993+YevheniiBiloshapka@users.noreply.github.com>
2022-10-16 12:42:43 +0300

    fix json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,7 @@ import { Component } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/statistics';
 import { Box, Section, Message } from './App.styled';
-
-export class App extends Component {
+const App = () => {
   state = {
     good: 0,
     neutral: 0,
@@ -14,23 +13,23 @@ export class App extends Component {
     this.setState(prevState => ({ [e]: prevState[e] + 1 }));
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    return (
-      <Box>
-        <FeedbackOptions
-          value={this.state}
-          incrementState={this.incrementState}
-        />
-        <Section>
-          <h2>Statistics</h2>
-          {good === 0 && (neutral === 0) & (bad === 0) ? (
-            <Message>😔 You have not left the feedback</Message>
-          ) : (
-            <Statistics value={this.state} />
-          )}
-        </Section>
-      </Box>
-    );
-  }
-}
+  const { good, neutral, bad } = this.state;
+  return (
+    <Box>
+      <FeedbackOptions
+        value={this.state}
+        incrementState={this.incrementState}
+      />
+      <Section>
+        <h2>Statistics</h2>
+        {good === 0 && (neutral === 0) & (bad === 0) ? (
+          <Message>😔 You have not left the feedback</Message>
+        ) : (
+          <Statistics value={this.state} />
+        )}
+      </Section>
+    </Box>
+  );
+};
+
+export default App;
